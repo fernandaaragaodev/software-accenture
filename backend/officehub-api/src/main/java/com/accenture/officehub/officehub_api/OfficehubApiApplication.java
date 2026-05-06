eeac6f890049ed1f5667257600e7b5168e75c99d
Danilo <danilo.almeida07@souunit.com.br>
2026-05-06 17:10:15 -0300

    Setup do projeto
    Apenas o esqueleto do back-end com spring boot(Java) e front-end com React(Typescript)

---
+++ backend/officehub-api/src/main/java/com/accenture/officehub/officehub_api/OfficehubApiApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.officehub.officehub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficehubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficehubApiApplication.class, args);
+	}
+
+}
---
+++ backend/officehub-api/src/test/java/com/accenture/officehub/officehub_api/OfficehubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.officehub.officehub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficehubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
